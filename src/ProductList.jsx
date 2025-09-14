788cd6da58c3be1d84c79d485be092c2f1fbdc03
dgodefroy <dgodefroy@7fois6.com>
2025-09-14 09:15:48 -0400

    fix navbar css

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -273,7 +273,7 @@ function ProductList({ onHomeClick }) {
     return (
         <div>
             <div className="navbar" style={styleObj}>
-                <div className="tag">
+                <div className="navbar-left tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
@@ -284,36 +284,34 @@ function ProductList({ onHomeClick }) {
                         </a>
                     </div>
 
-                </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} class="plant_heading" style={styleA}>Plants</a></div>
-                    <div> 
-                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>                            
-                            <text
-                                x="128"
-                                y="140"
-                                textAnchor="middle"
-                                dominantBaseline="middle"
-                                fontFamily="system-ui, -apple-system, Segoe UI, Roboto, Arial"
-                                fontWeight="700"
-                                fontSize="64"
-                                fill="#ffffff"
-                                pointerEvents="none"
-                                >
-                                {cartItems.items.length}
-                            </text>                               
-                            </svg></h1>
-                        </a>
-                    </div>
+                </div>                
+                <div className="navbar-center"> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div className="navbar-right"> 
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>                            
+                        <text
+                            x="128"
+                            y="140"
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                            fontFamily="system-ui, -apple-system, Segoe UI, Roboto, Arial"
+                            fontWeight="700"
+                            fontSize="64"
+                            fill="#ffffff"
+                            pointerEvents="none"
+                            >
+                            {cartItems.items.length}
+                        </text>                               
+                        </svg></h1>
+                    </a>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                     <div key={index}> {/* Unique key for each category div */}
-                        <h1>
-                        <div className="plantname_heading">{category.category}</div> {/* Display the category name */}
+                        <h1 className="plantname_heading plant_heading">
+                            {category.category} {/* Display the category name */}
                         </h1>
                         <div className="product-list"> {/* Container for the list of plant cards */}
                         {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
